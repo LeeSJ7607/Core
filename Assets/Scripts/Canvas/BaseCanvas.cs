ca056edf0c2cb3d221aadfe2e600b6b29d17ca29
LeeSJ <>
2024-04-16 21:25:11 +0900

    캔버스를 코어로 옮김
    유아이 매니저를 사용하지 않기로 결정함

--- Assets/Scripts/Canvas/BaseCanvas.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-internal abstract class BaseCanvas : MonoBehaviour
-{
-    
-}
---
+++ Assets/Scripts/Core/UI/Canvas/BaseCanvas.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+internal abstract class BaseCanvas : MonoBehaviour
+{
+    private readonly UIContainer _uiContainer = new();
+    private readonly Stack<UIPopup> _popups = new();
+    
+    protected virtual void Awake()
+    {
+        CreateCanvas();
+        CreateCanvasScale();
+        gameObject.AddComponent<GraphicRaycaster>();
+    }
+    
+    private void CreateCanvas()
+    {
+        var canvas = gameObject.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.worldCamera = Camera.main;
+        canvas.additionalShaderChannels = AdditionalCanvasShaderChannels.TexCoord1 
+          | AdditionalCanvasShaderChannels.Normal 
+          | AdditionalCanvasShaderChannels.Tangent;
+    }
+    
+    private void CreateCanvasScale()
+    {
+        var canvasScale = gameObject.AddComponent<CanvasScaler>();
+        canvasScale.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        canvasScale.referenceResolution = new Vector2(1920, 1080); //TODO: Const Table. (스크립터블 오브젝트)
+        canvasScale.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+        canvasScale.matchWidthOrHeight = 1f;
+    }
+    
+    public void OnTick()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PopPopup();
+        }
+    }
+    
+    private void PopPopup()
+    {
+        if (_popups.IsNullOrEmpty())
+        {
+            ShowPopup<UIPopup_System>();
+            return;
+        }
+
+        var popup = _popups.Pop();
+        popup.Hide();
+    }
+    
+    public T ShowPopup<T>() where T : UIPopup
+    {
+        var popup = _uiContainer.GetOrCreate<T>(transform);
+        popup.Show();
+        
+        _popups.Push(popup);
+        return popup;
+    }
+}
