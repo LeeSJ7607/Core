114eeb6486c92987a8af6464510334efc0dd583b
LeeSJ <>
2024-12-15 00:46:52 +0900

    [커맨드 패턴 추가] 로비에서 순차적으로 팝업을 보여줄 때 필요해서 넣음

---
+++ Command/Base/CommandStack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+internal sealed class CommandStack
+{
+    private readonly Stack<ICommand> _commands = new();
+    private ICommand _curCommand;
+
+    public void AddCommand(ICommand command)
+    {
+        _commands.Push(command);
+    }
+
+    public void Execute()
+    {
+        if (!Has())
+        {
+            return;
+        }
+
+        if (IsFinished())
+        {
+            return;
+        }
+
+        _curCommand = _commands.Pop();
+        _curCommand.Execute();
+    }
+
+    private bool Has()
+    {
+        return !_commands.IsNullOrEmpty();
+    }
+
+    private bool IsFinished()
+    {
+        return _curCommand != null && _curCommand.IsFinished();
+    }
+}
---
+++ Command/Base/ICommand.cs
@@ -0,0 +1,5 @@
+﻿internal interface ICommand
+{
+    void Execute();
+    bool IsFinished();
+}
