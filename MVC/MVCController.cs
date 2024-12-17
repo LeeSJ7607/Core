a7fd0d63663f72541345e01cdeab2d4450a3378f
LeeSJ <LeeSJ>
2024-12-17 20:02:33 +0900

    [MVC] 아키텍쳐 설계중

--- MVC/MVCController.cs
+++ MVC/MVCController.cs
@@ -3,14 +3,11 @@ using R3;
 
 internal abstract class MVCController : IDisposable
 {
-    protected readonly IMVCModel _model;
-    protected readonly IMVCView _view;
     protected readonly CompositeDisposable _disposable = new();
     
     protected MVCController(IMVCModel model, IMVCView view)
     {
-        _model = model;
-        _view = view;
+        
     }
 
     public void Dispose()
--- UI/Shop/ShopView.cs
+++ UI/Shop/ShopView.cs
@@ -1,10 +1,23 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 internal sealed class ShopView : IMVCView
 {
+    private Button _btn;
+
+    private void Awake()
+    {
+        _btn.AddClick(OnClick);
+    }
+    
     public void SetId(int id)
     {
         Debug.Log(id.ToString());
+    }
+
+    private void OnClick()
+    {
+        //TODO: ShopModel 데이터 변경 가능??
     }
 }
