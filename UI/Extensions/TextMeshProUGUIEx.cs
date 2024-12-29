a3de2f8087982907b6adc06d22d89426a048e13f
LeeSJ <LeeSJ>
2024-12-30 02:59:18 +0900

    로컬라이징 작업

--- UI/Extensions/TextMeshProUGUIEx.cs
+++ UI/Extensions/TextMeshProUGUIEx.cs
@@ -1,10 +1,42 @@
+using R3;
 using TMPro;
 
 internal sealed class TextMeshProUGUIEx : TextMeshProUGUI
 {
+    private CMOption _cmOption;
+    private readonly CompositeDisposable _disposable = new();
+    
+    protected override void OnDestroy()
+    {
+        _disposable.Dispose();
+        base.OnDestroy();
+    }
+
+    protected override void OnDisable()
+    {
+        _disposable.Clear();
+        base.OnDisable();
+    }
+    
     public override string text
     {
         get => base.text;
         set => base.text = value;
+    }
+
+    public void SetLocalizedText(int localizeId)
+    {
+        if (localizeId == 0)
+        {
+            text = $"localizeId is 0";
+            return;
+        }
+
+        _cmOption ??= ModelManager.Instance.Get<CMOption>();
+        _cmOption.LanguageChanged.Subscribe(_ =>
+        {
+            //TODO: 로컬라이징.
+            //text = localizeId;
+        }).AddTo(_disposable);
     }
 }
