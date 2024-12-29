290360172c3ec5492adb7652690e5530b5b6d257
LeeSJ <LeeSJ>
2024-12-30 03:11:48 +0900

    [LocalDB] 계정 변경 대응

--- Model/Model.cs
+++ Model/Model.cs
@@ -11,7 +11,6 @@ internal interface IReadOnlyModel
 
 public abstract class Model : IReadOnlyModel
 {
-    private static readonly string _savePath = $"{Application.persistentDataPath}/Model";
     private bool _canSave;
     
     protected abstract void OnRelease();
@@ -35,18 +34,24 @@ public abstract class Model : IReadOnlyModel
     {
         if (_canSave)
         {
-            FileUtil.SaveAsJson(_savePath, this);
+            FileUtil.SaveAsJson(CalcSavePath(), this);
         }
     }
 
     bool IReadOnlyModel.TryLoad(out IReadOnlyModel refModel)
     {
         var type = GetType();
-        var filePath = $"{_savePath}/{type.Name}";
+        var filePath = $"{CalcSavePath()}/{type.Name}";
         var model = (IReadOnlyModel)FileUtil.LoadFromJson(filePath, type);
 
         refModel = model;
         return model != null;
+    }
+    
+    private string CalcSavePath()
+    {
+        var cmUser = ModelManager.Instance.Get<CMUser>();
+        return $"{Application.persistentDataPath}{cmUser.UserId}/Model";
     }
 
     protected virtual void OnInitialize()
