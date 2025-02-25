aed6ff44a20b2b54d1bc6f5ce9b7fb1882964667
LeeSJ <>
2025-02-25 16:25:26 +0900

    [깃] 에디터에서 메뉴 선택을 통해 코어 깃을 클론할 수 있도록 기능 추가

---
+++ Editor/Menu/MenuGit.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Diagnostics;
+using UnityEditor;
+
+public sealed class MenuGit
+{
+    private const string SSH_CORE_GIT = "git@github.com:LeeSJ7607/Core.git";
+    private const string HTTPS_CORE_GIT = "https://github.com/LeeSJ7607/Core.git";
+    private const string CORE_FOLDER_PATH = "Assets/Scripts/Cor1";
+    
+    [MenuItem("Custom/Menu/Git/Clone Core")]
+    public static void CloneCore()
+    {
+        if (Directory.Exists(CORE_FOLDER_PATH))
+        {
+            var msg = $"Already Exists.\nPath: {CORE_FOLDER_PATH}";
+            EditorUtility.DisplayDialog("Git Clone Failed", msg, "OK");
+            return;
+        }
+        
+        var process = new Process()
+        {
+            StartInfo = new ProcessStartInfo()
+            {
+                FileName = "git",
+                Arguments = $"clone {SSH_CORE_GIT} {CORE_FOLDER_PATH}",
+                //Arguments = $"clone {HTTPS_CORE_GIT} {CORE_FOLDER_PATH}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+
+        process.Start();
+        process.WaitForExit();
+        
+        var stdoutx = process.StandardOutput.ReadToEnd();
+        var stderrx = process.StandardError.ReadToEnd();
+
+        if (process.ExitCode != 0)
+        {
+            var msg = $"The following error occurred during git clone.\n{stdoutx}\n{stderrx}";
+            EditorUtility.DisplayDialog("Git Clone Failed", msg, "OK");
+            process.Close();
+            return;
+        }
+        
+        EditorUtility.DisplayDialog("Git Clone Success", "Core Repository Clone Success", "OK");
+        AssetDatabase.Refresh();
+        process.Close();
+    }
+}
