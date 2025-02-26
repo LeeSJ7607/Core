58a8e13f7777cfa74048eab071860360599736eb
LeeSJ <>
2025-02-27 00:31:44 +0900

    [어드레서블] 파일이 변경될때마다 깃에 안 잡혀서 코드로 강제처리

---
+++ Editor/InitializeOnLoad/InitializeOnLoad_Addressable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+
+[InitializeOnLoad]
+internal static class InitializeOnLoad_Addressable
+{
+    static InitializeOnLoad_Addressable()
+    {
+        var settings = AddressableAssetSettingsDefaultObject.Settings;
+        settings.OnModification = (assetSettings, @event, obj) =>
+        {
+            AssetDatabase.SaveAssets();
+            Debug.Log($"Save Addressable.\nevent: {@event}, arg: {obj}");
+        };
+    }
+}
