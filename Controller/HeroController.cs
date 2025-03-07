1aef4d18c69c97db58b7aae43e2f4db9f93b3654
LeeSJ <>
2025-03-08 01:30:12 +0900

    [플레이어] 이동, 카메라 구현

---
+++ Controller/HeroController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public sealed class HeroController
+{
+    private readonly Unit _hero;
+    private readonly Camera _mainCam;
+    
+    public HeroController(Unit hero)
+    {
+        _hero = hero;
+        _mainCam = Camera.main ?? throw new System.NullReferenceException("Main camera is missing.");
+    }
+
+    public void OnUpdate()
+    {
+        UpdateCameraPos();
+        HandleMovement();
+    }
+
+    private void UpdateCameraPos()
+    {
+        var pos = _hero.transform.position;
+        // pos.y = 10f;
+        // pos.z -= 5f;
+        
+        _mainCam.transform.position = pos;
+    }
+
+    private void HandleMovement()
+    {
+        if (!TryGetHitPoint(out var pos))
+        {
+            return;
+        }
+    }
+
+    private bool TryGetHitPoint(out Vector3 pos)
+    {
+        pos = default;
+        
+        if (!Input.GetMouseButtonDown(0))
+        {
+            return false;
+        }
+        
+        const float maxDistance = 1000f;
+        var ray = _mainCam.ScreenPointToRay(Input.mousePosition);
+        if (!Physics.Raycast(ray, out var hit, maxDistance))
+        {
+            return false;
+        }
+
+        pos = hit.point;
+        pos.y = _hero.transform.position.y;
+        return true;
+    }
+}
--- Unit/Hero.cs
+++ Unit/Hero.cs
@@ -1,4 +1,16 @@
 ﻿internal sealed class Hero : Unit
 {
+    private HeroController _heroController;
     
+    protected override void Awake()
+    {
+        base.Awake();
+        _heroController = new HeroController(this);
+    }
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+        _heroController.OnUpdate();
+    }
 }
