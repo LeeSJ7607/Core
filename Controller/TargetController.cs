9ad12ad0ccd149a755410dfae2ebd4e8d3fa6f41
LeeSJ <>
2025-03-17 01:18:56 +0900

    [타겟 검색] 가장 가까운 적

--- Controller/TargetController.cs
+++ Controller/TargetController.cs
@@ -1,18 +1,20 @@
-﻿public sealed class TargetController
+﻿using System.Collections.Generic;
+
+public sealed class TargetController
 {
-    public Unit Target { get; private set; }
+    public IReadOnlyUnit Target { get; private set; }
     private readonly Seeker _seeker = new();
-    private readonly Unit _owner;
+    private readonly IReadOnlyUnit _owner;
     
-    public TargetController(Unit owner)
+    public TargetController(IReadOnlyUnit owner)
     {
         _owner = owner;
     }
     
-    public bool TryFindTarget(ESeekRule seekRule = ESeekRule.NearestEnemy)
+    public bool TryFindTarget(IEnumerable<IReadOnlyUnit> units, ESeekRule seekRule = ESeekRule.NearestEnemy)
     {
-        var targets = _seeker[seekRule].Seek(_owner);
-        if (targets == null)
+        var targets = _seeker[seekRule].Seek(units, _owner);
+        if (targets.IsNullOrEmpty())
         {
             return false;
         }
--- Seek/Rule/SeekByNearestEnemy.cs
+++ Seek/Rule/SeekByNearestEnemy.cs
@@ -1,6 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public sealed class SeekByNearestEnemy : ISeeker
 {
-    public IReadOnlyList<Unit> Seek(Unit owner) => throw new System.NotImplementedException();
+    public IReadOnlyList<IReadOnlyUnit> Seek(IEnumerable<IReadOnlyUnit> units, IReadOnlyUnit owner)
+    {
+        IReadOnlyUnit target = null;
+        var nearestDistance = float.MaxValue;
+        
+        foreach (var unit in units)
+        {
+            if (unit == owner)
+            {
+                continue;
+            }
+
+            if (unit.FactionType == owner.FactionType)
+            {
+                continue;
+            }
+            
+            var dis = Vector3.Distance(owner.Pos, unit.Pos);
+            if (dis >= nearestDistance)
+            {
+                continue;
+            }
+
+            nearestDistance = dis;
+            target = unit;
+        }
+
+        return target != null ? new [] { target } : Array.Empty<IReadOnlyUnit>();
+    }
 }
