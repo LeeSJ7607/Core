0ef9689180397931a7634439b7f41ae32fbc6d8f
LeeSJ <>
2025-03-29 02:27:06 +0900

    HierarchyTree 컴포넌트 등록시 자동으로 게임 오브젝트 이름 변경

---
+++ Editor/InitializeOnLoad/EditorInitializer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+
+[InitializeOnLoad]
+internal static class EditorInitializer
+{
+    static EditorInitializer()
+    {
+        AutoRenameHierarchyTree();
+        OnModificationAddressable();
+    }
+
+    private static void AutoRenameHierarchyTree()
+    {
+        ObjectFactory.componentWasAdded += (component) =>
+        {
+            if (component is not HierarchyTree)
+            {
+                return;
+            }
+
+            component.gameObject.name = component.GetType().Name;
+        };
+    }
+    
+    private static void OnModificationAddressable()
+    {
+        var settings = AddressableAssetSettingsDefaultObject.Settings;
+        settings.OnModification = (_, modificationEvent, obj) =>
+        {
+            EditorApplication.ExecuteMenuItem("File/Save Project");
+            Debug.Log($"Save Addressable.\nevent: {modificationEvent}, arg: {obj}");
+        };
+    }
+}
