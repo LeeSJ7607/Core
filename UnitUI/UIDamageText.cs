3f4e8019140f0f74d94336b339690ec7a914597e
LeeSJ <>
2025-04-06 03:15:10 +0900

    데미지 텍스트 연출 - 위로 올라가면서 서서히 사라짐

--- UnitUI/UIDamageText.cs
+++ UnitUI/UIDamageText.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 [RequireComponent(typeof(Billboard))]
 public sealed class UIDamageText : MonoBehaviour, IObjectPool
@@ -14,5 +15,13 @@ public sealed class UIDamageText : MonoBehaviour, IObjectPool
     public void Set(long damage)
     {
         _txtDamage.text = damage.ToStringMoney();
+
+        _txtDamage.alpha = 1f;
+        _txtDamage.DOFade(0f, LifeTime)
+                  .SetEase(Ease.Linear);
+        
+        transform.localPosition = Vector3.zero;
+        transform.DOMoveY(transform.position.y + 2f, LifeTime)
+                 .SetEase(Ease.Linear);
     }
 }
