53fae2200af49c256166bc6f1adc7265d1a3e17f
LeeSJ <>
2025-04-09 01:33:34 +0900

    에디터에서 특정 씬으로 이동하게끔 기능 추가

---
+++ Editor/Menu/MenuScene.cs
@@ -0,0 +1,64 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+internal sealed class MenuScene
+{
+    [MenuItem("Custom/Menu/Scene/Login", false, 0)]
+    private static void OpenLoginScene()
+    {
+        if (!TryFindScenePath(EScene.Login.ToString(), out var scenePath))
+        {
+            return;
+        }
+
+        EditorSceneManager.OpenScene(scenePath);
+    }
+    
+    [MenuItem("Custom/Menu/Scene/OutGame", false, 1)]
+    private static void OpenOutGameScene()
+    {
+        if (!TryFindScenePath(EScene.OutGame.ToString(), out var scenePath))
+        {
+            return;
+        }
+
+        EditorSceneManager.OpenScene(scenePath);
+    }
+    
+    [MenuItem("Custom/Menu/Scene/InGame", false, 2)]
+    private static void OpenInGameScene()
+    {
+        if (!TryFindScenePath(EScene.InGame.ToString(), out var scenePath))
+        {
+            return;
+        }
+
+        EditorSceneManager.OpenScene(scenePath);
+    }
+    
+    [MenuItem("Custom/Menu/Scene/Loading", false, 3)]
+    private static void OpenLoadingScene()
+    {
+        if (!TryFindScenePath(EScene.Loading.ToString(), out var scenePath))
+        {
+            return;
+        }
+
+        EditorSceneManager.OpenScene(scenePath);
+    }
+
+    private static bool TryFindScenePath(string sceneName, out string scenePath)
+    {
+        scenePath = null;
+        var guids = AssetDatabase.FindAssets($"{sceneName} t:Scene");
+
+        if (guids.IsNullOrEmpty())
+        {
+            EditorUtility.DisplayDialog("Scene Not Found", $"Scene named '{sceneName}' not found.", "OK");
+            return false;
+        }
+
+        scenePath = AssetDatabase.GUIDToAssetPath(guids[0]);
+        return true;
+    }
+}
