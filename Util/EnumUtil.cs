88c25fd2b1d45694f8841e796fb850fe938f48de
LeeSJ <>
2025-04-20 22:07:04 +0900

    유니티에 내장된 enum 들을 얻어올 때 "Obsolete" 은 제외하고 얻어오도록 함

---
+++ Util/EnumUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public static class EnumUtil
+{
+    public static string[] GetValidEnumNames<TEnum>() where TEnum : Enum
+    {
+        return Enum.GetValues(typeof(TEnum))
+                   .Cast<TEnum>()
+                   .Where(e => Convert.ToInt32(e) > -1)
+                   .Select(e => e.ToString()).ToArray();
+    }
+}
