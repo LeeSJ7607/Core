371c339a28814e10dec10146ee0972cc718b57f4
LeeSJ <LeeSJ>
2025-07-29 18:39:30 +0900

    네이밍 수정

--- Controller/SkillController.cs
+++ Controller/SkillController.cs
@@ -2,11 +2,11 @@ using System.Collections.Generic;
 
 public sealed class SkillController
 {
-    private readonly Dictionary<eSkillSlot, Skill> _skillMap = new();
+    private readonly Dictionary<eSkillSlot, Skill> _skillBySlotMap = new();
 
     public void OnUpdate()
     {
-        foreach (var skill in _skillMap.Values)
+        foreach (var skill in _skillBySlotMap.Values)
         {
             skill.OnUpdate();
         }
@@ -21,13 +21,13 @@ public sealed class SkillController
             var skillTable = TableManager.GetTable<SkillTable>().GetRow(skillId);
             var skill = SkillFactory.Create(skillTable.SkillInputType);
             skill.Initialize(skillTable);
-            _skillMap.Add(slotType, skill);
+            _skillBySlotMap.Add(slotType, skill);
         }
     }
 
     public void ShowIndicator(IAttacker owner, eSkillSlot slotType)
     {
-        if (!_skillMap.TryGetValue(slotType, out var skill))
+        if (!_skillBySlotMap.TryGetValue(slotType, out var skill))
         {
             return;
         }
@@ -48,7 +48,7 @@ public sealed class SkillController
 
     private bool TryGetPreparedSkill(out Skill refSkill)
     {
-        foreach (var skill in _skillMap.Values)
+        foreach (var skill in _skillBySlotMap.Values)
         {
             if (skill.EnableIndicator)
             {
